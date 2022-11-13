e5cfd4be6a088efa8204dabd64bc777d591efaec
Matthias Beyer <mail@beyermatthias.de>
2022-11-13 19:35:03 +0100

    Init flake
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ flake.nix
@@ -0,0 +1,135 @@
+{
+  description = "The deskodon app";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:matthiasbeyer/crane/0.8.0-master-fix-use-unsafe";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustTarget;
+
+        tomlInfo = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
+        inherit (tomlInfo) version;
+        pname = "deskodon";
+
+        src =
+          let
+            markdownFilter = path: _type: pkgs.lib.hasSuffix ".md" path;
+            filterPath = path: type: builtins.any (f: f path type) [
+              markdownFilter
+              craneLib.filterCargoSources
+              pkgs.lib.cleanSourceFilter
+            ];
+          in
+          pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = filterPath;
+          };
+
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src pname;
+
+          buildInputs = [
+            pkgs.pkg-config
+            pkgs.openssl
+            pkgs.cmake
+            pkgs.librsvg
+            pkgs.gtk3-x11
+            pkgs.libayatana-appindicator-gtk3
+          ];
+        };
+
+        deskodon = craneLib.buildPackage {
+          inherit cargoArtifacts src pname version;
+
+          cargoExtraArgs = "--all-features";
+
+          buildInputs = with pkgs; [
+            appimagekit
+            cargo-tauri
+            cmake
+            dbus
+            glib
+            gtk3
+            libayatana-appindicator-gtk3
+            librsvg
+            libsoup
+            openssl
+            pkg-config
+            webkitgtk
+          ];
+        };
+
+        cargo-check-everything = pkgs.writeScriptBin "cargo-check-everything" ''
+          #!${pkgs.runtimeShell}
+          ${rustTarget}/bin/cargo check --all --tests --examples --benches
+        '';
+
+      in
+      rec {
+        checks = {
+          inherit deskodon;
+          inherit cargo-check-everything;
+        };
+
+        packages.deskodon = deskodon;
+        packages.default = packages.deskodon;
+
+        apps.deskodon = flake-utils.lib.mkApp {
+          name = "deskodon";
+          drv = deskodon;
+        };
+        apps.default = apps.deskodon;
+
+        devShells.default = devShells.deskodon;
+        devShells.deskodon = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            appimagekit
+            dbus
+            glib
+            gtk3
+            libayatana-appindicator-gtk3
+            librsvg
+            libsoup
+            openssl
+            webkitgtk
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            rustTarget
+
+            cargo-check-everything
+
+            cargo-tauri
+            cargo-deny
+            gitlint
+            pkg-config
+            cmake
+          ];
+        };
+      }
+    );
+}
