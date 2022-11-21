6f671ad9977ad06ddaa57cd42939b1f88ad1d532
Matthias Beyer <mail@beyermatthias.de>
2022-11-21 08:59:29 +0100

    Make toot text smaller
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/app/toot.rs
+++ src/app/toot.rs
@@ -22,22 +22,22 @@ impl Toot {
     pub fn view(&self) -> Element<Message> {
         let header = Row::new()
             .spacing(20)
-            .push(Text::new(self.status.account.username.to_string()))
+            .push(Text::new(self.status.account.username.to_string()).size(12))
             .push(Text::new(
                 self.status
                     .reblog
                     .is_some()
                     .then(|| "Reblog")
                     .unwrap_or("Original"),
-            ))
-            .push(Text::new(self.status.created_at.to_string()));
+            ).size(12))
+            .push(Text::new(self.status.created_at.to_string()).size(12));
 
         let content = iced_native::widget::Row::new()
             .spacing(20)
-            .push(Text::new(self.status.content.to_string()));
+            .push(Text::new(self.status.content.to_string()).size(12));
 
-        let replies_count = Text::new(self.status.replies_count.to_string());
-        let reblogs_count = Text::new(self.status.reblogs_count.to_string());
+        let replies_count = Text::new(self.status.replies_count.to_string()).size(12);
+        let reblogs_count = Text::new(self.status.reblogs_count.to_string()).size(12);
 
         let footer = Row::new()
             .spacing(20)
