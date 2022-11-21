58b3c6c38064351adb377e7e6d9cbf6500292e77
Matthias Beyer <mail@beyermatthias.de>
2022-11-21 15:52:09 +0100

    Parse content HTML
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/app/toot.rs
+++ src/app/toot.rs
@@ -8,11 +8,19 @@ use super::message::Message;
 #[derive(Clone, Debug)]
 pub struct Toot {
     status: Arc<Status>,
+    content: scraper::Html,
 }
 
 impl From<Status> for Toot {
     fn from(status: Status) -> Self {
         Self {
+            content: {
+                if let Some(reblog) = status.reblog.as_ref() {
+                    scraper::Html::parse_fragment(&reblog.content)
+                } else {
+                    scraper::Html::parse_fragment(&status.content)
+                }
+            },
             status: Arc::new(status),
         }
     }
@@ -32,9 +40,44 @@ impl Toot {
             ).size(12))
             .push(Text::new(self.status.created_at.to_string()).size(12));
 
-        let content = iced_native::widget::Row::new()
-            .spacing(20)
-            .push(Text::new(self.status.content.to_string()).size(12));
+        let content = {
+            let elms = self.content.tree.values().filter_map(|node| {
+                match node {
+                    scraper::Node::Document => {
+                        tracing::trace!("Encountered element type Document");
+                        None
+                    },
+                    scraper::Node::Fragment => {
+                        tracing::trace!("Encountered element type Fragment");
+                        None
+                    }
+                    scraper::Node::Doctype(dt) => {
+                        tracing::trace!("Encountered element type Doctype {:?}", dt);
+                        None
+                    },
+                    scraper::Node::Comment(cm) => {
+                        tracing::trace!("Encountered element type Comment {:?}", cm);
+                        None
+                    },
+                    scraper::Node::Text(t) => {
+                        tracing::trace!("Encountered element type Text {:?}", t);
+                        Some(Text::new(t.to_string()).size(12))
+                    }
+                    scraper::Node::Element(el) => {
+                        tracing::trace!("Encountered element type Element {:?}", el);
+                        None
+                    },
+                    scraper::Node::ProcessingInstruction(pi) => {
+                        tracing::trace!("Encountered element type ProcessingInstruction {:?}", pi);
+                        None
+                    },
+                }
+            })
+            .map(Element::from)
+            .collect();
+
+            iced_native::widget::Column::with_children(elms).spacing(20)
+        };
 
         let replies_count = Text::new(self.status.replies_count.to_string()).size(12);
         let reblogs_count = Text::new(self.status.reblogs_count.to_string()).size(12);
