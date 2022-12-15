d5209e1f727ec06aee43bff5a6d9f00779d826df
Matthias Beyer <mail@beyermatthias.de>
2022-12-15 16:54:03 +0100

    Add frontend code for interfacing with backend
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- frontend/public/glue.js
+++ frontend/public/glue.js
@@ -3,3 +3,11 @@ const invoke = window.__TAURI_INVOKE__
 export async function invokeLogin(name) {
     return await invoke("login", {name: name});
 }
+
+export async function invokeGenerateAuth(instanceUrl) {
+    return await invoke("generate_auth", {instance: instanceUrl});
+}
+
+export async function invokeFetchAccessToken(instance, clientId, clientSecret, authToken) {
+    return await invoke("fetch_access_token", {instance: instance, client_id: clientId, client_secret: clientSecret, auth_token: authToken});
+}
--- frontend/src/tauri.rs
+++ frontend/src/tauri.rs
@@ -1,10 +1,25 @@
 use deskodon_types::login::LoginHandle;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
+use deskodon_types::access_token::AccessToken;
+use deskodon_types::auth::Auth;
+use url::Url;
+
 #[wasm_bindgen(module = "/public/glue.js")]
 extern "C" {
     #[wasm_bindgen(js_name = invokeLogin, catch)]
-    pub async fn login(name: String) -> Result<JsValue, JsValue>;
+    async fn login(name: String) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(js_name = invokeGenerateAuth, catch)]
+    async fn generate_auth(instance: String) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(js_name = invokeFetchAccessToken, catch)]
+    async fn fetch_access_token(
+        instance: String,
+        clientId: String,
+        clientSecret: String,
+        authToken: String,
+    ) -> Result<JsValue, JsValue>;
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -23,3 +38,21 @@ pub async fn call_login(name: String) -> Result<LoginHandle, Error> {
         .and_then(|val| serde_wasm_bindgen::from_value::<LoginHandle>(val).map_err(Error::from))
 }
 
+pub async fn call_generate_auth(instance: url::Url) -> Result<Auth, Error> {
+    generate_auth(instance.to_string())
+        .await
+        .map_err(|jsval| Error::Tauri(format!("{:?}", jsval)))
+        .and_then(|val| serde_wasm_bindgen::from_value::<Auth>(val).map_err(Error::from))
+}
+
+pub async fn call_fetch_access_token(auth: Auth, auth_token: String) -> Result<AccessToken, Error> {
+    fetch_access_token(
+        auth.url.to_string(),
+        auth.client_id,
+        auth.client_secret,
+        auth_token,
+    )
+    .await
+    .map_err(|jsval| Error::Tauri(format!("{:?}", jsval)))
+    .and_then(|val| serde_wasm_bindgen::from_value::<AccessToken>(val).map_err(Error::from))
+}
