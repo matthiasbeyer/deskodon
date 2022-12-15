80af4470b68723092835c627c8673a93d0f686db
Matthias Beyer <mail@beyermatthias.de>
2022-12-15 16:06:45 +0100

    Add logging plugin for tauri
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,17 @@
     windows_subsystem = "windows"
 )]
 
+use tauri_plugin_log::{LogTarget, LoggerBuilder};
 
 mod login;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(
+            LoggerBuilder::default()
+                .targets([LogTarget::Stdout])
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![crate::login::login])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
