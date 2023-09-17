b79f0e2c82511ba851023f2c6bd838b1aba04fe0
Matthias Beyer <mail@beyermatthias.de>
2023-09-17 20:56:39 +0200

    Add states for app
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- lib/src/command.rs
+++ lib/src/command.rs
@@ -1,6 +1,17 @@
 /// A command gets send from the frontend to the backend
 #[derive(Debug)]
 pub enum Command {
+    State(AppState),
+
     QuitApp,
     PostText { text: String },
+}
+
+#[derive(Debug)]
+pub enum AppState {
+    CreatingDefaultConfig,
+    LoadingConfig,
+    LoadingConfigFailed,
+    LoggedIn,
+    LoginFailed,
 }
